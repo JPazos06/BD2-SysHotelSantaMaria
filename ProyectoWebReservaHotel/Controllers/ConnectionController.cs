21584998350c6a52543085cf1f5d5a72ab27fb51
Joshua <chrpazosa@upt.pe>
2021-11-10 18:35:50 -0500

    software

---
+++ ProyectoWebReservaHotel/Controllers/ConnectionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoWebReservaHotel.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace ProyectoWebReservaHotel.Controllers
+{
+    public class ConnectionController
+    {
+        string conexion = new MongoClient("");
+    }
+}
